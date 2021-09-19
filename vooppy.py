e48fc13143a899b520fb8679df39d95d92c6dd2e
oilcake <oilpie@gmail.com>
2021-09-19 03:51:53 +0300

    video is still playing

--- vooppy.py
+++ vooppy.py
@@ -6,33 +6,40 @@ file_01 = 'A01Elsa.mov'
 file_02 = 'A10MMBAAHD.mov'
 unit = os.path.join(path, file_02)
 
-# Create a VideoCapture object and read from input file
-# If the input is the camera, pass 0 instead of the video file name
-while True:
-    cap = cv2.VideoCapture(unit)
 
-    # Check if camera opened successfully
-    if not cap.isOpened():
-        print("Error opening video stream or file")
+def play(video):
     # Read until video is completed
-    while(cap.isOpened()):
+    while(video.isOpened()):
         # Capture frame-by-frame
-        ret, frame = cap.read()
+        ret, frame = video.read()
         if ret:
             # Display the resulting frame
             cv2.imshow('Frame', frame)
             cv2.setWindowProperty('Frame', cv2.WND_PROP_TOPMOST, 1)
-
-            # Press Q on keyboard to  exit
-            if cv2.waitKey(5) & 0xFF == ord('q'):
-                break
-
-        # Break the loop
         else:
-            break
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        # Press Q on keyboard to  exit
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            # When everything done, release the video capture object
+            return cap.release()
 
-    # When everything done, release the video capture object
-    cap.release()
+
+
+
+# Create a VideoCapture object and read from input file
+# If the input is the camera, pass 0 instead of the video file name
+
+
+cap = cv2.VideoCapture(unit)
+amount_of_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+print(amount_of_frames)
+
+# Check if camera opened successfully
+if cap.isOpened():
+    while not play(cap) == cap.release():
+        play(cap)
+else:
+    print("Error opening video stream or file")
 
 # Closes all the frames
 cv2.destroyAllWindows()
